8b2ec75f560db96d2c8d79687e61484efef17d92
Winston Lee <winstonleemail@gmail.com>
2020-09-14 08:08:19 -0400

    Added documentation for the Cell class.

--- src/Cell.java
+++ src/Cell.java
@@ -3,15 +3,24 @@ import java.awt.*;
 import javax.swing.JComponent;
 import javax.swing.border.LineBorder;
 
+/*
+ * The base class representing a single cell in a grid.
+ * 
+ * @author Winston Lee
+ */
 public class Cell extends JComponent
 {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -3059067480908071499L;
 	private int x, y, width, height;
 
+	/*
+	 * Constructs a new Cell whose upper-left corner is specified as (x,y) and whose width and height are specified by the arguments of the same name.
+	 * 
+	 * @param x the specified X coordinate
+	 * @param y the specified Y coordinate
+	 * @param width the width of the Cell
+	 * @param height the height of the Cell
+	 */
 	public Cell(int x, int y, int width, int height)
 	{
 		this.x = x;
@@ -24,39 +33,68 @@ public class Cell extends JComponent
 		setBorder(new LineBorder(Color.WHITE));
 	}
 
-	//Returns true iff the given point is in this Box's bounding rectangle
+	/*
+	 * Checks whether this Cell "contains" the specified point, where x and y are defined to be relative to the coordinate system of this component.
+	 * 
+	 * @param x the x coordinate of the point
+	 * @param y the y coordinate of the point
+	 * @return true if this component logically contains x,y
+	 */
+	@Override
 	public boolean contains(int x, int y)
 	{
 		return getBounds().contains(x, y);
 	}
 
-	//Returns the x-coordinate.
+	/*
+	 * Gets the x-coordinate of this Cell's upper left hand corner.
+	 * 
+	 * @return the x-coordinate of this Cell's upper left hand corner
+	 */
 	public int getX()
 	{
 		return x;
 	}
 
-	//Returns the y-coordinate.
+	/*
+	 * Gets the y-coordinate of this Cell's upper left hand corner.
+	 * 
+	 * @return the y-coordinate of this Cell's upper left hand corner
+	 */
 	public int getY()
 	{
 		return y;
 	}
 
-	//Returns the width.
+	/*
+	 * Gets the width of this cell.
+	 * 
+	 * @return the width of this cell
+	 */
 	public int getWidth()
 	{
 		return width;
 	}
 
-	//Returns the height.
+	/*
+	 * Gets the height of this cell.
+	 * 
+	 * @return the height of this cell
+	 */
 	public int getHeight()
 	{
 		return height;
 	}
 	
-	//Draws the box interior.
-	public void draw(Graphics g)
+	/*
+	 * Paints the interior of this Cell.
+	 * 
+	 * @param g the the <code>Graphics</code> object to protect
+	 */
+	@Override
+	public void paintComponent(Graphics g)
 	{
+		super.paintComponent(g);
 		//To be implemented.
 	}
 	
