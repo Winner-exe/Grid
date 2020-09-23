5dfa93fd22d1bfd35df8a4c99724545f96b2fa66
Winston Lee <winstonleemail@gmail.com>
2020-09-23 10:58:28 -0400

    Added panel class.

--- src/SpriteDriver.java
+++ src/SpriteDriver.java
@@ -1,7 +1,14 @@
+import javax.swing.JFrame;
+
 public class SpriteDriver
 {
     public static void main(String[] args)
     {
+        JFrame frame = new JFrame("Sprite");
+        frame.setContentPane(new SpritePanel());
+        frame.setVisible(true);
+        frame.setFocusable(true);
+        frame.setSize(1280, 1280);
 
     }
 }
---
+++ src/SpritePanel.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SpritePanel extends JPanel implements ActionListener
+{
+    Sprite sprite;
+    Timer timer;
+
+    public SpritePanel()
+    {
+        sprite = new Sprite("sprite.png", 9, 6);
+        timer = new Timer(500, this);
+
+        this.repaint();
+        timer.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        this.repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        sprite.draw((Graphics2D) g, 50, 50, this);
+    }
+}
